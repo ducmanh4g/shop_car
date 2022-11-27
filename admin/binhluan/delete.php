1f2ea6efbaa77d9e185ba401f4f0ad3a7aab054f
ducmanh4g <vanmanh05042002@gmail.com>
2022-11-27 17:06:03 +0700

    d

--- admin/binhluan/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "../model/connect.php";
-    $id = $_GET["id"];
-    $query = "DELETE FROM binhluan WHERE idbl='$id'";
-    connect($query); 
-    header("Location:".$_SERVER['HTTP_REFERER']);
-?>
