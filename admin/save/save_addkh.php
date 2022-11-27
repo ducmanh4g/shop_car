40bf62f315dcac415128937b165081cdba40e38e
ducmanh4g <vanmanh05042002@gmail.com>
2022-11-27 17:01:40 +0700

    mới

---
+++ admin/save/save_addkh.php
@@ -0,0 +1,30 @@
+<?php 
+    include "../../admin/model/connect.php";
+
+    $hoten = $_GET['hoten'];
+    $taikhoan = $_GET['taikhoan'];
+    $matkhau = $_GET['matkhau'];
+    $sdt = $_GET['sdt'];
+    $gmail = $_GET['gmail'];
+    $quyen = $_GET['quyen'];
+    $sqltk = "select * from taikhoan";
+    $nguoidung = getAll($sqltk);
+    $count = "";
+    foreach ($nguoidung as $key => $value) {
+        if($taikhoan == $value['taikhoan']){
+            $count=$value['taikhoan'];
+            break;          
+         }
+    }
+    if(!$count){
+        $sql = "insert into taikhoan(gmail,taikhoan,matkhau,hoten,idquyen,sdt) values ('$gmail','$taikhoan','$matkhau','$hoten',$quyen,'$sdt')";
+        connect($sql);
+        header("Location:".$_SERVER['HTTP_REFERER']);
+    }   
+    else{
+        header("Location:".$_SERVER['HTTP_REFERER']);
+    }
+      
+    
+    
+?>
