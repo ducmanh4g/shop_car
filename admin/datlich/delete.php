5560a2fad36ed721014570c1435dd5d462907353
ducmanh4g <vanmanh05042002@gmail.com>
2022-11-27 17:01:02 +0700

    mới

--- admin/datlich/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "../model/connect.php";
-    $id = $_GET["id"];
-    $query = "DELETE FROM datlichxem WHERE iddl='$id'";
-    connect($query);
-   
-    header("Location:".$_SERVER['HTTP_REFERER']);
-?>
--- view/save/save_spcungloai.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    $spcungloai = $_GET['id'];
-    
-    $_SESSION['spcl'] = $spcungloai;
-    
-    header("Location:".$_SERVER['HTTP_REFERER']);
-?>
